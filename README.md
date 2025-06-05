0db316e887cc3cb7de4d385490e68fcdaaa772c2
alfan <smartfhand@gmail.com>
2025-06-05 10:48:14 +0700

    RND 1

---
+++ README.md
@@ -0,0 +1,3 @@
+Manual testing
+
+adb -s RF8N90WDYBR push "D:\Boxphone\ngasal.mp4" /sdcard/Download/
--- push_file.bat
+++ push_file.bat
@@ -4,23 +4,24 @@ setlocal enabledelayedexpansion
 set /p FILE_TO_PUSH=Masukkan nama file yang akan diupload: 
 
 if not exist "%FILE_TO_PUSH%" (
-    echo File "%FILE_TO_PUSH%" not found!
+    echo File "%FILE_TO_PUSH%" tidak ada!
     pause
     exit /b
 )
 
-set /p DEST_PATH=/sdcard/Download/
+set "DEST_PATH=/sdcard/Download/"
 
 echo.
-echo ==== Mengupload "%FILE_TO_PUSH%" ke semua device ====
+echo ==== Mengupload file "%FILE_TO_PUSH%" ke semua android ====
 
 for /f "skip=1 tokens=1" %%D in ('adb devices') do (
     if not "%%D"=="offline" if not "%%D"=="unauthorized" if not "%%D"=="" (
-        echo Pushing "%FILE_TO_PUSH%" to device %%D...
-        adb -s %%D push "%FILE_TO_PUSH%" "%DEST_PATH%"
+        echo.
+        echo ► Device: %%D
+        adb -s %%D push "%FILE_TO_PUSH%" "!DEST_PATH!"
     )
 )
 
 echo.
-echo ==== Done ====
+echo ==== Done pushing to all devices ====
 pause
